fdbb3c95e34a3c2055a6563257e1145a7710e3a2
Sam Habiel <sam.habiel@gmail.com>
2018-10-23 11:10:49 -0400

    ZISH error trap update; failsafe for $$MAXREC; Unit Tests

--- Kernel/Tests/ZOSVGUT5.m
+++ Kernel/Tests/ZOSVGUT5.m
@@ -1,8 +1,13 @@
-ZOSVGUT5 ; OSE/SMH - Unit Tests for GT.M VistA Port;Oct 22, 2018@10:32
+ZOSVGUT5 ; OSE/SMH - Unit Tests for GT.M VistA Port;Oct 23, 2018@10:29
  ;;8.0;KERNEL;**10004**;;
  ;
  D EN^%ut($t(+0),3)
  QUIT
+ ;
+STARTUP ;
+ ; ZEXCEPT: %UTSIMERR1
+ K %UTSIMERR1
+ QUIT 
  ;
 ZTMGRSET ; @TEST ZTMGRSET Rename GTM Routines
  D PATCH^ZTMGRSET(10004)
@@ -18,12 +23,12 @@ EC ; @TEST $$EC^%ZOSV
  D CHKTF^%ut($P(EC,",",4)["GVUNDEF")
  QUIT
  ;
-ZSY ; RUN ZSY with lsof in sbin
+ZSY ; @TEST RUN ZSY with lsof in sbin
  D ^ZSY
  D SUCCEED^%ut
  QUIT
  ;
-ACTJ ; Use of $T +0
+ACTJ ; @TEST Use of $T +0
  D CHKTF^%ut($$ACTJ^%ZOSV)
  QUIT
  ;
@@ -32,6 +37,13 @@ PATCH ; @TEST $$PATCH^XPDUTL, which prv accepted only 3 digits
  QUIT
  ;
 MAXREC ; @TEST $$MAXREC^%ZISH - $T +0
+ ; ZEXCEPT: %UTSIMERR1
  D CHKTF^%ut($$MAXREC^%ZISH("^DD"))
  D CHKTF^%ut($$MAXREC^%ZISH($NA(^TMP("SAM",$J))))
+ S %UTSIMERR1=1
+ N % S %=$$FTG^%ZISH("/usr/include","stdlib.h",$NA(^TMP($J,1,0)),2,"VVV")
+ D CHKEQ^%ut(%,0)
+ K %UTSIMERR1
+ N % S %=$$FTG^%ZISH("/usr/include","stdlib.h",$NA(^TMP($J,1,0)),2,"VVV")
+ D CHKEQ^%ut(%,1)
  QUIT
