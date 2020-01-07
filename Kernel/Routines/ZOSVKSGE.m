54ee47b9fab2b34f832f5d568c69dab2dc8920ce
Sam Habiel <sam.habiel@gmail.com>
2020-01-07 16:11:39 -0500

    SAGG Data Collection was too slow.
    It turns out that most of the time was being spent in the grep calls.
    Now we refactored that code so that instead of grepping, we create an
    array containing the number of sets and kills. This is much faster, as
    we only have go through the file once rather than an estimated 800
    times.

--- Kernel/Routines/ZOSVKSGE.m
+++ Kernel/Routines/ZOSVKSGE.m
@@ -1,4 +1,4 @@
-%ZOSVKSE ;YDB/CJE&OSE/SMH - ZOSVKSGE - Global data (GT.M) ;Sep 17, 2018@17:56
+%ZOSVKSE ;YDB/CJE&OSE/SMH - ZOSVKSGE - Global data (GT.M) ;2020-01-07  12:18 PM
  ;;8.0;KERNEL;**10003**;Jul 26, 2004;Build 48
  ;
  Q
@@ -46,14 +46,38 @@ ALLOS ;-- entry point now for all OS's
  ; Extract the journal
  n jExtractFile s jExtractFile=$$DUMPJOUR()
  ;
+ ; Get Sets and Kills for journal
+ if jExtractFile'="" do SK(jExtractFile)
+ ;
  ; Run the global analysis
  D ALLGLO(jExtractFile)
+ ;
  ;
  ; Delete the journal
  N % S %=$$RETURN^%ZOSV("rm -f "_jExtractFile,1)
  S ^XTMP("KMPS",KMPSSITE,NUM,KMPSDT,"KEY")="blocks_U_blockSize_U_bytes_U_adj_U_reg_U_acc_U_jourstr_U_jourfile_U_sets_U_kills"
  ;
  Q
+ ;
+SK(jExtractFile,sk) ; [Private] Sets and Kills for each global from the journal
+ ; ZEXCEPT: GLOARRAY,readonly
+ ; 0x06a01828 [0x0078] :: SET    \65374,58595\145206723\4286364787\6014\0\0\0\0\0\0\^TMP(6014,2803,0)="CHEYENNE REGIONAL MED CENTER^56^^^^^^^^^N"$
+ ; 0x06a018a0 [0x0070] :: SET    \65374,58595\145206724\336001251\6014\0\0\0\0\0\0\^TMP(6014,2804,0)="CRISP HEALTH AND WVHIN^24^^^^^^^^^N"$
+ ; 0x06a01910 [0x0068] :: SET    \65374,58595\145206725\504442487\6014\0\0\0\0\0\0\^TMP(6014,2805,0)="CVS MINUTE CLINIC^44^^^^^^^^^N"$
+ ; 0x06a01978 [0x0070] :: SET    \65374,58595\145206726\2549882539\6014\0\0\0\0\0\0\^TMP(6014,2806,0)="CEDARS-SINAI HEALTH SYSTEM^6^^^^^^^^^N"$
+ ; 0x06a019e8 [0x0068] :: SET    \65374,58595\145206727\2897577692\6014\0\0\0\0\0\0\^TMP(6014,2807,0)="CONNECTVIRGINIA^51^^^^^^^^^N"$
+ o jExtractFile:(readonly:rewind):0
+ e  quit
+ u jExtractFile
+ n x f  r x:0 q:$zeof  do
+ . n isSet   s isSet=x[":: SET"
+ . n isKill s isKill=x[":: KILL"
+ . i ('isSet)&('isKill) quit
+ . n glo s glo=$p(x,"^",2),glo=$p(glo,"("),glo=$p(glo,"="),glo="^"_glo
+ . if isSet,$i(GLOARRAY(glo,"set"))
+ . if isKill,$i(GLOARRAY(glo,"kill"))
+ c jExtractFile
+ QUIT
  ;
 DUMPJOUR() ; [$$ Private] Dump default current journal file for grepping (only DEFAULT region)
  ; Output: File containing the extract
@@ -64,7 +88,7 @@ DUMPJOUR() ; [$$ Private] Dump default current journal file for grepping (only D
  n jourstr
  i jour<2 s jourstr="off" ; (2 active, 1 enabled but off; 0 disabled; -1 error)
  e  s jourstr="on"
- if jourstr'="on" quit
+ if jourstr'="on" quit ""
  ;
  ; Get Journal File
  n jourfile s jourfile=$view("JNLFILE",reg)
@@ -84,14 +108,16 @@ DUMPJOUR() ; [$$ Private] Dump default current journal file for grepping (only D
  q outfile
  ;
 ALLGLO(jExtractFile) ;- collect global info
+ ; ZEXCEPT: GLOARRAY
  N GLO S GLO=""
- ;
  F  S GLO=$O(GLOARRAY(GLO)) Q:GLO=""  D  Q:+$G(^XTMP("KMPS","STOP"))
  . o "pipe":(shell="/bin/sh":command="$gtm_dist/mupip size -sel="_GLO)::"pipe"
  . u "pipe"
  . n out
  . n x f  r x:1 q:$zeof  i x["Total" s out=x
  . c "pipe"
+ . ;
+ . if '$d(out)#2 quit
  . ;
  . ;out: Total               2               0               0 ~   0% 4096
  . ;
@@ -127,10 +153,9 @@ ALLGLO(jExtractFile) ;- collect global info
  . i jourstr="on" s jourfile=$view("JNLFILE",reg)
  . ;
  . ; If we have a journal file, get the # of sets/kills for the global
- . n sets,kills s (sets,kills)=0
- . i jourstr="on" do
- .. s sets=$$RETURN^%ZOSV("grep -c ""SET.*"_GLO_"("" "_jExtractFile)
- .. s kills=$$RETURN^%ZOSV("grep -c ""KILL.*"_GLO_"("" "_jExtractFile)
+ . n sets,kills
+ . s sets=+$g(GLOARRAY(GLO,"set"))
+ . s kills=+$g(GLOARRAY(GLO,"kill"))
  . ; 
  . ; Set info into global
  . S ^XTMP("KMPS",KMPSSITE,NUM,KMPSDT,GLO,KMPSZU)=blocks_U_blockSize_U_bytes_U_adj_U_reg_U_acc_U_jourstr_U_jourfile_U_sets_U_kills
